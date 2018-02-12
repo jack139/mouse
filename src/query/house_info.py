f26b41c1ba3b74342ba2a0613d82f8714e8f6e3b
GT <gt@f8geek.com>
2018-02-12 17:47:16 +0800

    update

---
+++ src/query/house_info.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+import web
+import time
+from bson.objectid import ObjectId
+from config import setting
+import helper
+
+db = setting.db_web
+
+# 管理员鼠笼信息
+
+url = ('/query/house_info')
+
+class handler:
+
+    def GET(self):
+        if not helper.logged(helper.PRIV_USER|helper.PRIV_GRP_ADMIN|helper.PRIV_TUTOR, 'QUERY'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data = web.input(house_id='')
+
+        if user_data['house_id']=='':
+            return render.info('参数错误！')  
+
+        # 当前课题组的实验员列表
+        group_list = helper.get_session_group_list()
+        db_user=db.user.find({
+            'privilege'  : helper.PRIV_USER, 
+            'group_list' : '' if len(group_list)==0 else group_list[0],
+        }).sort([('_id',1)])
+
+        house_data=db.house.find_one({
+            'house_id': user_data.house_id,
+            #'uname'   : helper.get_session_uname(),
+        })
+        if house_data is None:
+            # 不存在的鼠笼
+            return render.info('不存在的鼠笼！')  
+
+        # 此笼的小鼠信息
+        db_mice = db.mouse.find({
+            'house_id' : user_data['house_id'], 
+            'status'   : {'$nin' : ['killed', 'dead']}
+        }).sort([('_id',1)])
+
+        mice = [i for i in db_mice]
+
+        # 获取鼠笼数据
+        house = []
+        db_sku = db.house.find({
+            'group_id'    : helper.get_session_group_list()[0], # 只显示当前实验组的鼠笼
+            # 要增加条件，现在过期的鼠笼
+        }).sort([('house_id',1)])
+        for i in db_sku:
+            # 需要检查鼠笼类型的限制
+            if i['house_id']!=user_data['house_id']:
+                house.append(i['house_id'])
+
+        return render.query_house_info(helper.get_session_uname(), helper.get_privilege_name(), 
+            house_data, db_user, helper.HOUSE_TYPE, mice, helper.MOUSE_STATUS, house)
+
+
---
+++ src/query/shelf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import web
+from config import setting
+import helper
+
+db = setting.db_web
+
+# 笼架列表
+
+url = ('/query/shelf')
+
+PAGE_SIZE = helper.PAGE_SIZE
+
+#  -------------------
+class handler:  
+    def GET(self):
+        if not helper.logged(helper.PRIV_USER|helper.PRIV_GRP_ADMIN|helper.PRIV_TUTOR, 'QUERY'):
+            raise web.seeother('/')
+
+        render = helper.create_render()
+        user_data=web.input(page='0')
+
+        if not user_data['page'].isdigit():
+            return render.info('参数错误！')  
+
+        # 获取课题组名字对照
+        r3 = db.groups.find({'status':1})
+        group_name = {'n/a':'n/a'}
+        for i in r3:
+            group_name[i['group_id']]=i['name']
+
+        group_list = helper.get_session_group_list()
+
+        # 分页获取数据
+        db_sku = db.shelf.find({'group_id' : '' if len(group_list)==0 else group_list[0]},
+            sort=[('shelf_id', 1)],
+            limit=PAGE_SIZE,
+            skip=int(user_data['page'])*PAGE_SIZE
+        )
+
+        num = db_sku.count()
+        if num%PAGE_SIZE>0:
+            num = num / PAGE_SIZE + 1
+        else:
+            num = num / PAGE_SIZE
+        
+        return render.query_shelf(helper.get_session_uname(), helper.get_privilege_name(), 
+            db_sku, range(0, num), group_name)
