52cc2f7253e8958c4761477a8b1bd0c45691ac5d
GT <gt@f8geek.com>
2018-03-22 18:30:15 +0800

    同时输入出生日期

--- static/js/user_house_info.js
+++ static/js/user_house_info.js
@@ -143,8 +143,10 @@ $(function(){
         
         var house_id = $("#house_id").val();
 
-        alertify.prompt('请输入', '请输入新生小鼠数量', '',
-            function(evt, value){ 
+        alertify.prompt2('请输入',
+            function(evt, value, value2){ 
+                alertify.success(value2);
+
                 $.ajax({
                     type: "POST",
                     url: "/grp_user/newborn",
@@ -173,4 +175,132 @@ $(function(){
 
             }, function(){ } ); 
     });
+});
+
+
+
+
+alertify.dialog('prompt2', function () {
+    var input = document.createElement('INPUT');
+    var input2 = document.createElement('INPUT');
+    var p = document.createElement('P');
+    var p2 = document.createElement('P');
+
+    function clearContents(element){
+        while (element.lastChild) {
+            element.removeChild(element.lastChild);
+        }
+    }
+
+    return {
+        main: function (_title, _onok, _oncancel) {
+            var title, message, value, onok, oncancel;
+
+            title = _title;
+            message = "新生小鼠数量";
+            message2 = "出生日期（格式：yyyymmdd）";
+            value = '';
+            onok = _onok;
+            oncancel = _oncancel;
+
+            this.set('title', title);
+            this.set('message', message);
+            this.set('value', value);
+            this.set('onok', onok);
+            this.set('oncancel', oncancel);
+            return this;
+        },
+        setup: function () {
+            return {
+                buttons: [
+                    {
+                        text: alertify.defaults.glossary.ok,
+                        key: 13,
+                        className: alertify.defaults.theme.ok,
+                    },
+                    {
+                        text: alertify.defaults.glossary.cancel,
+                        key: 27,
+                        invokeOnClose: true,
+                        className: alertify.defaults.theme.cancel,
+                    }
+                ],
+                focus: {
+                    element: input,
+                    select: true
+                },
+                options: {
+                    maximizable: false,
+                    resizable: false
+                }
+            };
+        },
+        build: function () {
+            input.className = alertify.defaults.theme.input;
+            input.setAttribute('type', 'text');
+            input.value = this.get('value');
+            input2.className = alertify.defaults.theme.input;
+            input2.setAttribute('type', 'text');
+            input2.value = this.get('value');
+            this.elements.content.appendChild(p);
+            this.elements.content.appendChild(input);
+            this.elements.content.appendChild(p2);
+            this.elements.content.appendChild(input2);
+        },
+        prepare: function () {
+            //nothing
+        },
+        setMessage: function (message) {
+            clearContents(p);
+            p.innerHTML = "新生小鼠数量";
+            p2.innerHTML = "出生日期（格式：yyyymmdd）";
+        },
+        settings: {
+            message: undefined,
+            labels: undefined,
+            onok: undefined,
+            oncancel: undefined,
+            value: '',
+            type:'text',
+            reverseButtons: undefined,
+        },
+        settingUpdated: function (key, oldValue, newValue) {
+            switch (key) {
+            case 'message':
+                this.setMessage(newValue);
+                break;
+            case 'value':
+                input.value = newValue;
+                break;
+            case 'type':
+                input.type = 'text';
+                break;
+            }
+        },
+        callback: function (closeEvent) {
+            var returnValue;
+            switch (closeEvent.index) {
+            case 0:
+                this.settings.value = input.value;
+                if (typeof this.get('onok') === 'function') {
+                    returnValue = this.get('onok').call(this, closeEvent, this.settings.value, input2.value);
+                    if (typeof returnValue !== 'undefined') {
+                        closeEvent.cancel = !returnValue;
+                    }
+                }
+                break;
+            case 1:
+                if (typeof this.get('oncancel') === 'function') {
+                    returnValue = this.get('oncancel').call(this, closeEvent);
+                    if (typeof returnValue !== 'undefined') {
+                        closeEvent.cancel = !returnValue;
+                    }
+                }
+                if(!closeEvent.cancel){
+                    input.value = this.settings.value;
+                }
+                break;
+            }
+        }
+    };
 });
